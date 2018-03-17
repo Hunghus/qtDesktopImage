96e68de61a3f22447bd91f341b023f0b3de609c1
Đặng Tiến Quy <quydt@sandinh.net>
2018-03-17 22:33:02 +0700

    Code 17/03

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QPixmap>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap pix("C:/Users/Public/Pictures/Sample Pictures/Desert.jpg");
+    ui->label_pic->setPixmap(pix.scaled(ui->label_pic->width(),ui->label_pic->height(), Qt::KeepAspectRatio));
+
 }
 
 MainWindow::~MainWindow()
